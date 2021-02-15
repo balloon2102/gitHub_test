306cb9d9da92740827c2b85d6bdba4578fe73ea0
balloon <dhozu1@gmail.com>
2021-02-15 12:19:19 +0900

    add weight

--- copy_and_rename.py
+++ copy_and_rename.py
@@ -89,5 +89,29 @@ def dup_and_remane_chain(src='', tgt= '', parent = True):
         b.select = False
         b.select_head = False
         b.select_tail = False
-        
+
+
+
+def symmetry_weight(src_vt_sfx = 'R', tgt_vt_sfx = 'L'):#필터링할 서픽스
+    vt_list =  bpy.context.selected_objects[0].vertex_groups.items()
+    for vt in range(len(vt_list)):
+        bpy.context.selected_objects[0].vertex_groups.active_index = vt #이로서 버텍스 그룹을 순차적으로 선택 할 수 있다. 순차적으로 선택 
+        sel_vtg = bpy.context.object.vertex_groups.active.name #현제 활성화된  버텍스 그룹의 이름을 안다.  
+        if tgt_vt_sfx in sel_vtg :
+            trg_vtg_name = bpy.context.object.vertex_groups.active.name.replace( tgt_vt_sfx , src_vt_sfx ) #서픽스를 찾은후 변경한다.
+            trg_vtg_ind = bpy.context.object.vertex_groups[trg_vtg_name].index #윗줄에서 서픽스를 바꾼 반대쪽 버텍스 그룹의 인덱스를 확인한다.
+
+            bpy.ops.object.vertex_group_copy() #버텍스 그룹을 카피한다.
+            bpy.ops.object.vertex_group_mirror(mirror_weights=True, flip_group_names=False, all_groups=False, use_topology=False) #버텍스 그룹을 미러 시킨다. 
+            
+            bpy.context.object.vertex_groups.active_index  = trg_vtg_ind #이미 기존에 서픽스가 붙어있는 반대쪽 버텍스그룹을 활성화 시킨다. 
+            bpy.ops.object.vertex_group_remove() #삭제를시킨다.
+            
+            cp_vtg = bpy.context.object.vertex_groups[sel_vtg + "_copy" ].index #처음 버텍스를 미러시킬때 기존에 이미 미러상태의 버텍스 그룹이 있었기 때문에, 서픽스가 갱신된다. 
+            print (cp_vtg)
+            bpy.context.object.vertex_groups.active_index = cp_vtg #따라서 이 버텍스그룹의 이름을 수정시켜 줘야한다.
+            trg_vtg_name = bpy.context.object.vertex_groups.active.name = trg_vtg_name #정상적으로 수정을 완료한다.     
+
+
+
 #dup_and_remane_chain(src='CTRL', tgt= 'MCH', parent = False)
